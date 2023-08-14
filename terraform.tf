1aefd62ee1de1b2380ce387ff21901a42f5c3420
Sunny Cheema <sunnycheema89@hotmail.com>
2023-08-13 17:08:13 -0700

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,13 @@
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  /*cloud {
+    organization = "suncheezy"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
